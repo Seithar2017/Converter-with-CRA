b50856763d1869ff5fec2552bad92eaff549152e
Krzysztof Waszkiewicz <kwaszkiewicz94@gmail.com>
2020-08-19 11:43:32 +0200

    add unitInput structure

--- src/components/UnitInput.js
+++ src/components/UnitInput.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 const UnitInput = (props) => {
-  return <div>UnitInput</div>;
+  return (
+    <form>
+      <label htmlFor="from">From: </label>
+      <input list="from" />
+      <datalist id="from">
+        <option value="CM"></option>
+        <option value="M"></option>
+      </datalist>
+
+      <label htmlFor="To">To: </label>
+      <input list="To" />
+      <datalist id="To">
+        <option value="CM"></option>
+        <option value="M"></option>
+      </datalist>
+      <button>Convert</button>
+    </form>
+  );
 };
 
 export default UnitInput;
