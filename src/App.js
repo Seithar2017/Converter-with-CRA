af211a0006182ea28e57584c1512d4e51c86f52e
Krzysztof Waszkiewicz <kwaszkiewicz94@gmail.com>
2020-08-20 11:33:56 +0200

    extend database and converter operations base

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import UnitInput from "./components/UnitInput";
 import ResultOutput from "./components/ResultOutput";
 
 class App extends Component {
-  units = ["kg", "dag", "gram", "stone", "tone"];
+  units = [
+    [false, "kg", "dag", "gram", "stone", "tone"],
+    [false, "pl", "usd", "euro", "gbp"],
+  ];
 
   state = {
     activeFrom: "",
@@ -34,24 +37,36 @@ class App extends Component {
     });
   };
   convert = (uF, uT, amount) => {
+    debugger;
     if (uF === uT) return amount;
-    const kg = this.convertToKg(uF);
-    const conversion = this.convertAnyFromKg(uT, kg);
+    const main = this.convertToMainUnit(uF);
+    const conversion = this.convertAnyFromMain(uT, main);
     return amount * conversion;
   };
-  convertAnyFromKg = (uT, kg) => {
-    if (uT === "kg") return kg;
-    else if (uT === "dag") return kg * 100;
-    else if (uT === "gram") return kg * 1000;
-    else if (uT === "stone") return kg * 0.157473044;
-    else if (uT === "tone") return kg * 0.0001;
+  convertAnyFromMain = (uT, main) => {
+    debugger;
+    if (uT === "main") return main;
+    else if (uT === "dag") return main * 100;
+    else if (uT === "gram") return main * 1000;
+    else if (uT === "stone") return main * 0.157473044;
+    else if (uT === "tone") return main * 0.0001;
+    else if (uT === "usd") return main * 3.72;
+    else if (uT === "euro") return main * 4.4;
+    else if (uT === "gbp") return main * 4.87;
   };
-  convertToKg = (uF) => {
+  convertToMainUnit = (uF) => {
+    debugger;
+    // The main unit as weight is the KG unit
     if (uF === "dag") return 0.01;
     else if (uF === "gram") return 0.001;
     else if (uF === "stone") return 6.35029318;
     else if (uF === "tone") return 1000;
     else if (uF === "kg") return 1;
+    //The main unit as cash is the PL unit
+    else if (uF === "usd") return 0.27;
+    else if (uF === "euro") return 0.23;
+    else if (uF === "gbp") return 0.21;
+    else if (uF === "pl") return 1;
   };
   render() {
     return (
