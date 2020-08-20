1b53bc6900a77a25f7833c894b4a3c32a32b8415
Krzysztof Waszkiewicz <kwaszkiewicz94@gmail.com>
2020-08-20 10:00:00 +0200

    datalist->select input, add updateResult

--- src/components/UnitInput.js
+++ src/components/UnitInput.js
@@ -6,27 +6,79 @@ class UnitInput extends Component {
     activeFrom: "",
     activeTo: "",
   };
+
+  handleChange = (e) => {
+    const name = e.target.name;
+    this.setState({
+      [name]: e.target.value,
+    });
+  };
+
+  handleClick = (e) => {
+    const name = e.target.name;
+    this.setState({
+      [name]: "",
+    });
+    console.log(e.target);
+  };
+
+  componentDidUpdate() {
+    if (this.props.state.activeFrom !== this.state.activeFrom) {
+      this.props.unitChangeFrom(this.state.activeFrom);
+    }
+    if (this.props.state.activeTo !== this.state.activeTo) {
+      this.props.unitChangeTo(this.state.activeTo);
+    }
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { activeFrom, activeTo, amount } = this.props.state;
+    if (this.state.activeFrom && this.state.activeTo && amount > 0) {
+      const result = this.props.convert(activeFrom, activeTo, amount);
+      this.props.updateResult(result);
+    } else {
+      this.props.updateResult("Incorrect values");
+    }
+  };
   render() {
+    const measures = this.props.database.map((measure) => {
+      return (
+        <option key={measure} value={measure}>
+          {measure}
+        </option>
+      );
+    });
+
     return (
       <form>
         <div className="from">
           <label htmlFor="from">From: </label>
-          <input list="from" />
-          <datalist id="from">
-            <option value="CM"></option>
-            <option value="M"></option>
-          </datalist>
+          <select
+            list="from"
+            name="activeFrom"
+            value={this.state.activeFrom}
+            onChange={this.handleChange}
+            id="from"
+          >
+            <option style={{ display: "none" }} disabled label=" "></option>
+            {measures}
+          </select>
         </div>
 
         <div className="to">
           <label htmlFor="To">To: </label>
-          <input list="To" />
-          <datalist id="To">
-            <option value="CM"></option>
-            <option value="M"></option>
-          </datalist>
+          <select
+            list="to"
+            name="activeTo"
+            value={this.state.activeTo}
+            onChange={this.handleChange}
+            id="to"
+          >
+            <option style={{ display: "none" }} disabled label=" "></option>
+            {measures}
+          </select>
         </div>
-        <button>Convert</button>
+        <button onClick={this.handleSubmit}>Convert</button>
       </form>
     );
   }
